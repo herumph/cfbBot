8d42a1756aca5bb20c001082e83e91cb0000a7fd
Scott Davis <sfdavis0@gmail.com>
2021-07-29 23:00:00 -0400

    file formatting

--- cfbBot.py
+++ cfbBot.py
@@ -7,60 +7,63 @@ import pandas as pd
 
 # Functions to read and write files into arrays.
 def get_array(input_string):
-    with open("textfiles/"+input_string+".txt","r") as f:
+    with open("textfiles/" + input_string + ".txt", "r") as f:
         input_array = f.readlines()
     input_array = [x.strip("\n") for x in input_array]
-    return(input_array)
+    return input_array
 
-def write_out(input_string,input_array):
-    with open("textfiles/"+input_string+".txt","w") as f:
+
+def write_out(input_string, input_array):
+    with open("textfiles/" + input_string + ".txt", "w") as f:
         for i in input_array:
-            f.write(i+"\n")
+            f.write(i + "\n")
     return
+
 
 # Big mess of a function to actually respond to commands
 def call_bot(body, author, contributors):
-    reply=""
+    reply = ""
 
-    if (body.count("WHOA!")):
+    if body.count("WHOA!"):
         return "he has trouble with the snap!"
 
-    if (body.count("!post")):
+    if body.count("!post"):
         return "posted!"
 
     # CFB belt "location" for a given date
-    if(body.count("!belt")):
+    if body.count("!belt"):
         indices = [i for i, x in enumerate(body) if x == "!belt"]
         for i in indices:
-            date = body[i+1]
+            date = body[i + 1]
             reply += getBelt(date)
 
     return reply
 
+
 def getBelt(date):
     try:
-        date = datetime.strptime(date, '%Y/%m/%d')
+        date = datetime.strptime(date, "%Y/%m/%d")
     except:
         try:
-            date = datetime.strptime(date, '%Y-%m-%d')
+            date = datetime.strptime(date, "%Y-%m-%d")
         except:
             return "Your date formatting is incorrect. Please use YYYY-MM-DD or YYYY/MM/DD format."
 
-    if date >= datetime.strptime("1/1/1940", '%m/%d/%Y'):
-        df = pd.read_csv('D1Games_1869-2019.csv')
-        df['Fulldate'] = [datetime.strptime(x, '%Y-%m-%d') for x in df['Fulldate']]
-        df = df.sort_values('Fulldate')
-        df = df[(df['Fulldate'] >= datetime.strptime('1940/01/01', '%Y/%m/%d'))]
+    if date >= datetime.strptime("1/1/1940", "%m/%d/%Y"):
+        df = pd.read_csv("D1Games_1869-2019.csv")
+        df["Fulldate"] = [datetime.strptime(x, "%Y-%m-%d") for x in df["Fulldate"]]
+        df = df.sort_values("Fulldate")
+        df = df[(df["Fulldate"] >= datetime.strptime("1940/01/01", "%Y/%m/%d"))]
 
         print(date)
         beltWearer = "USC"
         for ind, row in df.iterrows():
             # early exit condition
-            if row['Fulldate'] > date:
+            if row["Fulldate"] > date:
                 return beltWearer
 
-            elif row['Team'] == beltWearer and row['Result'] == 'L':
-                beltWearer = row['Opponent']
+            elif row["Team"] == beltWearer and row["Result"] == "L":
+                beltWearer = row["Opponent"]
         return beltWearer
 
     else:
--- cfbReddit.py
+++ cfbReddit.py
@@ -3,14 +3,16 @@ import praw
 from config_bot import *
 
 # Reddit stuff
-r = praw.Reddit(user_agent = "cfbBot 0.0.1 by herumph",
-    client_id = ID,
-    client_secret = SECRET,
-    username = REDDIT_USERNAME,
-    password = REDDIT_PASS)
+r = praw.Reddit(
+    user_agent="cfbBot 0.0.1 by herumph",
+    client_id=ID,
+    client_secret=SECRET,
+    username=REDDIT_USERNAME,
+    password=REDDIT_PASS,
+)
 
 sub = "cfbsecret"
-#sub = "RumphyBot"
+# sub = "RumphyBot"
 subreddit = r.subreddit(sub)
 contributors = []
 
@@ -19,41 +21,41 @@ already_done = cfbBot.get_array("already_done")
 
 # Looking at comments
 for comment in r.subreddit(sub).comments(limit=25):
-  if(comment.id not in already_done and str(comment.author) != "cfbBot"):
-    # marking comment as read if it's a reply
-    # this ensures that the PM part does not respond as well
-    comment.mark_read()
-    already_done.append(comment.id)
-    # Making sure the already_done file doesn't get too big.
-    del already_done[0]
-    cfbBot.write_out("already_done",already_done)
+    if comment.id not in already_done and str(comment.author) != "cfbBot":
+        # marking comment as read if it's a reply
+        # this ensures that the PM part does not respond as well
+        comment.mark_read()
+        already_done.append(comment.id)
+        # Making sure the already_done file doesn't get too big.
+        del already_done[0]
+        cfbBot.write_out("already_done", already_done)
 
-    # Saving comment
-    comment_list = str(comment.body)
-    comment_list = comment_list.split()
+        # Saving comment
+        comment_list = str(comment.body)
+        comment_list = comment_list.split()
 
-    # passing off to get reply and replying
-    reply = cfbBot.call_bot(comment_list, comment.author, contributors)
+        # passing off to get reply and replying
+        reply = cfbBot.call_bot(comment_list, comment.author, contributors)
 
-    if reply == "posted!":
-        title = "WHOA!"
-        selftext = "he has trouble with the snap!"
-        subreddit.submit(title=title,selftext=selftext,send_replies=False)
+        if reply == "posted!":
+            title = "WHOA!"
+            selftext = "he has trouble with the snap!"
+            subreddit.submit(title=title, selftext=selftext, send_replies=False)
 
-    if reply != "":
-        comment.reply(reply)
+        if reply != "":
+            comment.reply(reply)
 
 
 # sorting through unread DMs
 for pm in r.inbox.unread(limit=25):
-  pm.mark_read()
-  # only responding to pm's, not comment replies
-  if(not pm.was_comment):
-    body = str(pm.body)
-    body = body.split()
+    pm.mark_read()
+    # only responding to pm's, not comment replies
+    if not pm.was_comment:
+        body = str(pm.body)
+        body = body.split()
 
-    reply = cfbBot.call_bot(body, pm.author, contributors)
+        reply = cfbBot.call_bot(body, pm.author, contributors)
 
-  # Responding
-  if(len(reply) > 1):
-    pm.author.message(pm.subject, reply)
+    # Responding
+    if len(reply) > 1:
+        pm.author.message(pm.subject, reply)
